5cdf5ec2ab21cbded5b137bf20e2490986921bbf
Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
2021-08-15 12:52:30 +0200

    bbc.com initial scrape
    - Successfully scrapes first group of articles on the website
    
    - Scraping returns article title and article link

--- scraper.py
+++ scraper.py
@@ -1,4 +1,56 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
+
+# ---------------------------------------------------------------------------
+
+
+siteName = "https://www.bbc.com"
+sitePrefix = "https://www.bbc"
+
+bbcHtmlFile = requests.get(siteName).text
+soupBbcHtmlFile = BeautifulSoup(bbcHtmlFile, "lxml")
+
+
+articlesContainer = soupBbcHtmlFile.find("div", class_ = "module__content")
+articlesList = articlesContainer.find("ul", class_ = "media-list")
+articlesListItems = articlesList.findAll("li", class_ = "media-list__item")
+
+
+for list in articlesListItems:
+    mediaContent = list.find("div", class_ = "media__content")
+    mediaLink = mediaContent.find("a" ,class_ = "media__link")
+    
+    articleLink = ""
+    articleTitle = mediaLink.text
+
+
+    if (sitePrefix not in mediaLink["href"]):
+        articleLink = siteName + mediaLink["href"]
+
+    else:
+        articleLink = mediaLink["href"]
+    
+
+
+    print(articleTitle)
+    print(articleLink)
+    
+
+
+#print(articlesContainer.prettify())
+
+
+
+    
+    
+
+
+
+
+
+
+
+
 
 
