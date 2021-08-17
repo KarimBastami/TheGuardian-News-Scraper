2d846e15b321185a764007443b1b4f3d06a17686
Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
2021-08-17 23:13:52 +0200

    Fixed scraping stopping half way through
    - Ignored unwanted sections
    
    - improved outer layer scraping criteria (using a new pattern found that's more consistent)

--- scraper.py
+++ scraper.py
@@ -14,34 +14,40 @@ soupBbcHtmlFile = BeautifulSoup(bbcHtmlFile, "lxml")
 
 for articlesContainer in soupBbcHtmlFile.findAll("div", class_ = "module__content"):
 
-    articlesList = articlesContainer.find("ul", class_ = "media-list")
-    articlesListItems = articlesList.findAll("li", class_ = "media-list__item")
+    try:
+        articlesList = articlesContainer.find("ul")
+        articlesListItems = articlesList.select("li[class *= --]")
 
 
-    for list in articlesListItems:
-        mediaContent = list.find("div", class_ = "media__content")
-        mediaLink = mediaContent.find("a" ,class_ = "media__link")
-        articleLinkSuffix = mediaLink["href"].strip()
+        for list in articlesListItems:
 
-        
+            mediaContent = list.find("div", class_ = "media__content")
 
-        articleLink = ""
-        articleTitle = mediaLink.text.strip()
+            try:
+                mediaLink = mediaContent.find("a" ,class_ = "media__link")
+                articleLinkSuffix = mediaLink["href"].strip()
+            except:
+                continue
+            
 
-
-        if (sitePrefix not in articleLinkSuffix):
-            articleLink = siteName + articleLinkSuffix
-
-        else:
-            articleLink = articleLinkSuffix
-        
+            articleLink = ""
+            articleTitle = mediaLink.text.strip()
 
 
-        print(articleTitle)
-        print(articleLink)
-        print("----------------------------------------------\n")
+            if (sitePrefix not in articleLinkSuffix):
+                articleLink = siteName + articleLinkSuffix
+
+            else:
+                articleLink = articleLinkSuffix
+            
+
+
+            print(articleTitle)
+            print(articleLink)
+            print("----------------------------------------------\n")
     
-
+    except:
+        continue
 
 
 
