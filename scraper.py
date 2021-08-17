54ff9e6ad7a686442eae70eb72061a6a00debdee
Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
2021-08-18 01:22:58 +0200

    REFACTORING - OVERALL MUCH CLEANER CODE
    - Separated functionality of getting relevant html containers , parsing for links and parsing for titles into individual functions
    
    - All functions return lists of their respective items / elements

--- scraper.py
+++ scraper.py
@@ -8,47 +8,103 @@ import csv
 siteName = "https://www.bbc.com"
 sitePrefix = "https://www.bbc"
 
-bbcHtmlFile = requests.get(siteName).text
-soupBbcHtmlFile = BeautifulSoup(bbcHtmlFile, "lxml")
+htmlFile = requests.get(siteName).text
+soupHtmlFile = BeautifulSoup(htmlFile, "lxml")
 
 
-for articlesContainer in soupBbcHtmlFile.findAll("div", class_ = "module__content"):
 
-    try:
-        articlesList = articlesContainer.find("ul")
-        articlesListItems = articlesList.select("li[class *= --]")
-
-
-        for list in articlesListItems:
-
-            mediaContent = list.find("div", class_ = "media__content")
-
-            try:
-                mediaLink = mediaContent.find("a" ,class_ = "media__link")
-                articleLinkSuffix = mediaLink["href"].strip()
-            except:
-                continue
-            
-
-            articleLink = ""
-            articleTitle = mediaLink.text.strip()
-
-
-            if (sitePrefix not in articleLinkSuffix):
-                articleLink = siteName + articleLinkSuffix
-
-            else:
-                articleLink = articleLinkSuffix
-            
-
-
-            print(articleTitle)
-            print(articleLink)
-            print("----------------------------------------------\n")
+def getArticleLinkContainers(_soupHtmlFile):
     
-    except:
-        continue
+    mediaContainers = []
 
+    for articlesContainer in _soupHtmlFile.findAll("div", class_ = "module__content"):
+
+        try:
+            articlesList = articlesContainer.find("ul")
+            articlesListItems = articlesList.select("li[class *= --]")
+
+
+            for list in articlesListItems:
+
+                mediaContent = list.find("div", class_ = "media__content")
+
+                try:
+                    mediaLink = mediaContent.find("a" ,class_ = "media__link")
+                except:
+                    continue
+                
+                
+                mediaContainers.append(mediaLink)
+
+        except:
+            continue
+    
+    return mediaContainers
+
+
+
+
+def getArticlesLinks(_siteName, _sitePrefix, _soupHtmlFile):
+
+    articleLinkList = []
+    linkContainers = getArticleLinkContainers(_soupHtmlFile)
+
+    for link in linkContainers:
+        
+        try:
+            articleLinkSuffix = link["href"].strip()
+        except:
+            continue
+
+
+        articleLink = ""
+
+        if (_sitePrefix not in articleLinkSuffix):
+            articleLink = _siteName + articleLinkSuffix
+
+        else:
+            articleLink = articleLinkSuffix
+        
+
+        articleLinkList.append(articleLink)
+
+    return articleLinkList
+
+
+
+
+def getArticlesTitles(_soupHtmlFile):
+
+    articleTitleList = []
+    linkContainers = getArticleLinkContainers(_soupHtmlFile)
+
+    for link in linkContainers:
+
+        try:
+            articleTitle = link.text.strip()
+        except:
+            continue
+
+        articleTitleList.append(articleTitle)
+
+    return articleTitleList
+
+
+
+
+# ----------------------------------- #
+# ------------- Testing ------------- #
+# ----------------------------------- #
+
+for titles in getArticlesTitles(soupHtmlFile):
+    print(titles)
+    print("-------------")
+
+print("")
+
+for links in getArticlesLinks(siteName, sitePrefix, soupHtmlFile):
+    print(links)
+    print("-------------")
 
 
 
