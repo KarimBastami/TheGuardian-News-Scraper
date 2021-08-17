f24109758c7f1baec0bdd0eb26b619ea9f2a8569
Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
2021-08-17 22:08:11 +0200

    Obtained first half of articles containers
    - not all containers were obtained due to inconsistencies in html

--- scraper.py
+++ scraper.py
@@ -12,32 +12,34 @@ bbcHtmlFile = requests.get(siteName).text
 soupBbcHtmlFile = BeautifulSoup(bbcHtmlFile, "lxml")
 
 
-articlesContainer = soupBbcHtmlFile.find("div", class_ = "module__content")
-articlesList = articlesContainer.find("ul", class_ = "media-list")
-articlesListItems = articlesList.findAll("li", class_ = "media-list__item")
+for articlesContainer in soupBbcHtmlFile.findAll("div", class_ = "module__content"):
+
+    articlesList = articlesContainer.find("ul", class_ = "media-list")
+    articlesListItems = articlesList.findAll("li", class_ = "media-list__item")
 
 
-for list in articlesListItems:
-    mediaContent = list.find("div", class_ = "media__content")
-    mediaLink = mediaContent.find("a" ,class_ = "media__link")
-    articleLinkSuffix = mediaLink["href"].strip()
+    for list in articlesListItems:
+        mediaContent = list.find("div", class_ = "media__content")
+        mediaLink = mediaContent.find("a" ,class_ = "media__link")
+        articleLinkSuffix = mediaLink["href"].strip()
+
+        
+
+        articleLink = ""
+        articleTitle = mediaLink.text.strip()
 
 
-    articleLink = ""
-    articleTitle = mediaLink.text.strip()
+        if (sitePrefix not in articleLinkSuffix):
+            articleLink = siteName + articleLinkSuffix
+
+        else:
+            articleLink = articleLinkSuffix
+        
 
 
-    if (sitePrefix not in articleLinkSuffix):
-        articleLink = siteName + articleLinkSuffix
-
-    else:
-        articleLink = articleLinkSuffix
-    
-
-
-    print(articleTitle)
-    print(articleLink)
-    print("----------------------------------------------")
+        print(articleTitle)
+        print(articleLink)
+        print("----------------------------------------------\n")
     
 
 
