82c84c7603ac950fb96555dfaac867bd24d6dd91
Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
2021-08-20 21:35:15 +0200

    Code updated to work with TheGuardian.com

--- scraper.py
+++ scraper.py
@@ -2,7 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
+from requests.api import get
+
 # ---------------------------------------------------------------------------
+
+
+
+def removeDuplicates(list):
+    uniqueList = []
+
+    for item in list:
+        if item not in uniqueList:
+            uniqueList.append(item)
+    
+    return uniqueList
 
 
 
@@ -16,18 +29,17 @@ def getArticleLinkContainers(_soupHtmlFile):
     
     mediaContainers = []
 
-    for articlesContainer in _soupHtmlFile.findAll("div", class_ = "module__content"):
+    for articlesContainer in _soupHtmlFile.findAll("div", class_ = "fc-slice-wrapper"):
 
         try:
             articlesList = articlesContainer.find("ul")
-            articlesListItems = articlesList.select("li[class *= --]")
+            articlesListItems = articlesList.findAll("li", class_="fc-slice__item")
 
             for list in articlesListItems:
 
-                mediaContent = list.find("div", class_ = "media__content")
-
                 try:
-                    mediaLink = mediaContent.find("a" ,class_ = "media__link")
+                    mediaContent = list.find("h3", class_ = "fc-item__title")
+                    mediaLink = mediaContent.find("a" ,class_ = "fc-item__link")
                 except:
                     continue
                                 
@@ -35,6 +47,7 @@ def getArticleLinkContainers(_soupHtmlFile):
 
         except:
             continue
+    
     
     return mediaContainers
 
@@ -65,6 +78,9 @@ def getArticlesLinks(_siteUrl, _sitePrefix, _soupHtmlFile):
 
         articleLinkList.append(articleLink)
 
+
+    articleLinkList = removeDuplicates(articleLinkList)
+    print("No of URLs: ",len(articleLinkList))
     return articleLinkList
 
 
@@ -78,62 +94,65 @@ def getArticlesTitles(_soupHtmlFile):
     for link in linkContainers:
 
         try:
-            articleTitle = link.text.strip()
+            articleTitle = link.find("span", class_ = "js-headline-text").text.strip()
         except:
             continue
 
         articleTitleList.append(articleTitle)
 
+
+    articleTitleList = removeDuplicates(articleTitleList)
+    print("No of Titles: ", len(articleTitleList))
     return articleTitleList
 
 
 
 
-def getArticlesAuthors(_siteUrl, _sitePrefix, _soupHtmlFile):
 
-    articlesAuthorList = []
-    articlesUrls = getArticlesLinks(_siteUrl, _sitePrefix, _soupHtmlFile)
-    authorSearchParameters = ["lx-commentary__meta-reporter",
-                              "ssrcss-1gg9z89-Contributor",
-                              "qa-contributor-name"]
 
-    count = 0
+# def getArticlesAuthors(_siteUrl, _sitePrefix, _soupHtmlFile):
 
-    for url in articlesUrls:
-        articlePage = getHtmlFileFromUrl(url)
-        author = ""
+#     articlesAuthorList = []
+#     articlesUrls = getArticlesLinks(_siteUrl, _sitePrefix, _soupHtmlFile)
+#     authorSearchParameters = ["lx-commentary__meta-reporter",
+#                               "ssrcss-1gg9z89-Contributor",
+#                               "qa-contributor-name"]
+
+#     count = 0
+
+#     for url in articlesUrls:
+#         articlePage = getHtmlFileFromUrl(url)
+#         author = ""
         
-        try:
-            authorList = articlePage.findAll("p", class_ = authorSearchParameters)
+#         try:
+#             authorList = articlePage.findAll("p", class_ = authorSearchParameters)
 
-            for auth in authorList:
-                author = auth.text
+#             for auth in authorList:
+#                 author = auth.text
                 
-                print(author)
-                print(url)
-                print("--------------------------------------------------------------------")
+#                 print(author)
+#                 print(url)
+#                 print("--------------------------------------------------------------------")
 
-            #if (count == 3):
-               # break
-            #count += 1
+#             #if (count == 3):
+#                # break
+#             #count += 1
 
-        except:
-            author = "No Author Found"
+#         except:
+#             author = "No Author Found"
 
-            print(author)
-            print(url)
-            print("--------------------------------------------------------------------")
+#             print(author)
+#             print(url)
+#             print("--------------------------------------------------------------------")
             
         
-        print("--------------------------------------------------------------------")
-        print("--------------------------------------------------------------------")
-        print("--------------------------------------------------------------------")
+#         print("--------------------------------------------------------------------")
+#         print("--------------------------------------------------------------------")
+#         print("--------------------------------------------------------------------")
         
-        articlesAuthorList.append(author)
+#         articlesAuthorList.append(author)
     
-    return articlesAuthorList
-
-
+#     return articlesAuthorList
 
 
 
@@ -145,10 +164,15 @@ def getArticlesAuthors(_siteUrl, _sitePrefix, _soupHtmlFile):
 # ----------------------------------- #
 
 
-siteUrl = "https://www.bbc.com"
-sitePrefix = "https://www.bbc"
+siteUrl = "https://www.theguardian.com/international"
+sitePrefix = "https://www.theguardian"
 
 soupHtmlFile = getHtmlFileFromUrl(siteUrl)
+
+
+# for i in getArticleLinkContainers(soupHtmlFile):
+#     print(i.prettify())
+
 
 
 # for titles in getArticlesTitles(soupHtmlFile):
@@ -162,8 +186,9 @@ soupHtmlFile = getHtmlFileFromUrl(siteUrl)
 #     print("-------------")
 
 
-getArticlesAuthors(siteUrl, sitePrefix, soupHtmlFile)
 
+getArticlesTitles(soupHtmlFile)
+getArticlesLinks(siteUrl, sitePrefix, soupHtmlFile)
     
     
 
