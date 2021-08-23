62d3baffc503e1176993f45491f76bd9221755b9
Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
2021-08-23 11:46:00 +0200

    Only new articles can be add to the database

--- mongo.py
+++ mongo.py
@@ -5,17 +5,40 @@ import scraper
 
 
 
+def getUniqueArticleIndices(_titleList, _collection):
+    indicesList = []
+
+    for i in range(0, len(_titleList)):
+        match = _collection.find_one({"Title": _titleList[i]})
+
+        if (not match):
+            indicesList.append(i)
+        
+    return indicesList
+            
+        
+        
+
+
+
 
 # ----------------------------------- #
 # -------------   Main  ------------- #
 # ----------------------------------- #
+
+client = pymongo.MongoClient("mongodb+srv://NewsScraper:mrbOkWRipWEGh1Bt@news-cluster.fvtga.mongodb.net/News?retryWrites=true&w=majority")
+
+db = client["News"]
+collection = db["TheGuardian"]
+
+# -----------------------------------------------------------------------------------------------
+
 
 
 siteUrl = "https://www.theguardian.com/international"
 sitePrefix = "https://www.theguardian"
 
 soupHtmlFile = scraper.getHtmlFileFromUrl(siteUrl)
-
 
 
 articleTitles = scraper.getArticlesTitles(soupHtmlFile)
@@ -25,24 +48,25 @@ articleUrls = scraper.getArticlesLinks(siteUrl, sitePrefix, soupHtmlFile)
 articleTextContent = scraper.getArticlesText(siteUrl, sitePrefix, soupHtmlFile)
 
 
+# -----------------------------------------------------------------------------------------------
 
 
-client = pymongo.MongoClient("mongodb+srv://NewsScraper:mrbOkWRipWEGh1Bt@news-cluster.fvtga.mongodb.net/News?retryWrites=true&w=majority")
 
-db = client["News"]
-article = db["TheGuardian"]
+uniqueArticleIndexList = getUniqueArticleIndices(articleTitles, collection)
 
 for i in range(len(articleTitles)):
 
-    row = {"Title": "", "Category": "", "Author": "", "Url" : "", "Text": ""}
+    if (i in uniqueArticleIndexList):
 
-    row["Title"] = articleTitles[i]
-    row["Category"] = articleCategories[i]
-    row["Author"] = articleAuthors[i]
-    row["Url"] = articleUrls[i]
-    row["Text"] = articleTextContent[i]
+        row = {"Title": "", "Category": "", "Author": "", "Url" : "", "Text": ""}
 
-    article.insert_one(row)
+        row["Title"] = articleTitles[i]
+        row["Category"] = articleCategories[i]
+        row["Author"] = articleAuthors[i]
+        row["Url"] = articleUrls[i]
+        row["Text"] = articleTextContent[i]
+
+        collection.insert_one(row)
 
     
 
